c39e1e7d23e1d8739af6c247d4ee90b423ce5be1
mohankumar <rmk3200@gmail.com>
2022-09-12 16:08:18 +0530

    Initial project commit

---
+++ my-project/android/app/src/main/java/com/rmktech/cap/MainActivity.java
@@ -0,0 +1,5 @@
+package com.rmktech.cap;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ my-project/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.rmktech.cap',
+  appName: 'my-project',
+  webDir: 'dist/capacitor',
+  bundledWebRuntime: false
+};
+
+export default config;
